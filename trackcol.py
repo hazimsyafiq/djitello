0ea6155291883c4f8627ce6fc08b9def791e61b5
H. Syafiq <46275483+hazimsyafiq@users.noreply.github.com>
2021-01-07 22:27:13 +0800

    Create trackcol.py

---
+++ trackcol.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(1)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+
+deadZone=100
+global imgContour
+
+def empty(a):
+    pass
